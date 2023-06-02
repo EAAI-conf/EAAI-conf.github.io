c90845f4818c35049569d94909b1924371d52b99
EAAI-conf <135246410+EAAI-conf@users.noreply.github.com>
2023-06-02 19:48:37 -0400

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,47 +1,13 @@
-#!/usr/bin/env node
-var optimist = require('optimist');
-var commander = require('commander');
-var args = optimist.argv;
-var fs = require('fs');
-var colors = require('colors');
+
+/* Highlight */
+$( document ).ready(function() {
+    hljs.initHighlightingOnLoad();
+    $('table').addClass('table table-striped table-hover');
+});
 
 
 
-if(args.new){
-	if(args.new && args.from){
-		var from = require(__dirname+'/bases/'+args.from+'.json');
-
-		fs.writeFile(args.new,from.content,function(err){
-			console.log(args.new + ' Created'.green+' From ' + args.from.green)
-		});
-	}else{
-		fs.writeFile(args.new,'',function(err){
-			console.log(args.new + ' Created'.green)
-		});
-	}
-
-
-	
-}
-
-if(args.save && args.as){
-	fs.readFile(args.save,'utf8', function(err,data){
-		var file = {};
-		file.content = data;
-		if(!err){
-			fs.writeFile(__dirname+'/bases/'+args.as+'.json',JSON.stringify(file), function(err){
-				console.log(args.as + 'Saved!'.green);
-			});
-		}
-	});
-}
-
-
-
-
-
-
-
-
-
-
+/* Prevent disabled links from causing a page reload */
+$("li.disabled a").click(function() {
+    event.preventDefault();
+});
